16df4665b4b395ffacec58a03254c605232ca824
Stuart Garner <stuart@radialaspect.com>
2016-09-23 19:51:16 -0400

    Adding Leap Year exercism project

---
+++ leap/leap.rb
@@ -0,0 +1,17 @@
+module BookKeeping
+  VERSION = 2
+end
+
+class Year
+
+  def self.leap?(year)
+    if year % 4 != 0
+      false
+    elsif (year % 100 == 0) && (year % 400 != 0)
+      false
+    else
+      true
+    end
+  end
+
+end
