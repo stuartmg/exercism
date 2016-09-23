8959c57e333c7c0e506affd3808425e0d8118a6c
Stuart Garner <stuart@radialaspect.com>
2016-09-23 18:42:35 -0400

    Adding ability to track previously used names across all Robot instances and ensure that the are not used again

--- robot-name/robot_name.rb
+++ robot-name/robot_name.rb
@@ -4,16 +4,23 @@ end
 
 class Robot
 
+  @@previous_names = []
+
   def initialize
     @letters = ('A'..'Z').to_a
+    reset
   end
 
   def name
-    @name ||= generate_name
+    @name
   end
 
   def reset
-    @name = nil
+    while @@previous_names.include?(@name)
+      @name = generate_name
+    end
+
+    @@previous_names << @name
   end
 
   private
