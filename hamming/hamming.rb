cb667b8a079df964cb115f7dace411de7ad843ff
Stuart Garner <stuart@radialaspect.com>
2016-09-23 14:29:22 -0400

    changing to just loop through the first nucleotide strand and compare it to the 2nd strand instead of zipping strands together before comparing

--- hamming/hamming.rb
+++ hamming/hamming.rb
@@ -7,17 +7,7 @@ class Hamming
   def self.compute(strand1, strand2)
     raise ArgumentError if strand1.length != strand2.length
 
-    nucleotide_pairs(strand1, strand2).count { |n| n[0] != n[1] }
-  end
-
-  private
-
-  def self.nucleotide_pairs(strand1, strand2)
-    nucleotides(strand1).zip(nucleotides(strand2))
-  end
-
-  def self.nucleotides(strand)
-    strand.chars
+    (0..strand1.length).count { |i| strand1[i] != strand2[i] }
   end
 
 end
