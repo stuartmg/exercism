84f7e42c0c921bf00de0d062d8ec9a4880899592
Stuart Garner <stuart@radialaspect.com>
2016-09-25 17:31:03 -0400

    making some minor enhancements to make the code cleaner

--- series/series.rb
+++ series/series.rb
@@ -1,13 +1,13 @@
 class Series
 
   def initialize(number)
-    @numbers = number.chars.map { |c| c.to_i }
+    @numbers = number.chars.map(&:to_i)
   end
 
   def slices(length)
     raise ArgumentError, "Slice too large" if length > @numbers.length
 
-    @numbers.each_cons(length).map { |slice| slice }
+    @numbers.each_cons(length).to_a
   end
 
 end
