17a74c3ff1a3bc75127b6aad7e3b1c0c3621d2f1
Stuart Garner <stuart@radialaspect.com>
2016-09-26 23:25:55 -0400

    Changing to an improved regex which eliminated a lot of code

--- acronym/acronym.rb
+++ acronym/acronym.rb
@@ -5,17 +5,7 @@ end
 class Acronym
 
   def self.abbreviate(phrase)
-    abbr = phrase.scan(/\w+/).map do |word|
-      next if word.empty?
-
-      if word.upcase == word || word.downcase == word
-        word[0].upcase
-      else
-        word.gsub(/[a-z]/, '').upcase
-      end
-    end
-
-    abbr.join
+    phrase.scan(/[A-Z]+[a-z]*|[a-z]+/).map { |word| word[0].upcase }.join
   end
 
 end
