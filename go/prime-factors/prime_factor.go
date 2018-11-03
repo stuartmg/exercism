9fdf1346fe2ad733c4d59183f4575540d83d05eb
Stuart Garner <stuart@radialaspect.com>
2018-11-03 08:02:08 -0400

    Adding go prime-factors exercise

---
+++ go/prime-factors/prime_factor.go
@@ -0,0 +1,14 @@
+package prime
+
+func Factors(num int64) []int64 {
+	factors := make([]int64, 0, 5)
+
+	for i := int64(2); i <= num; i++ {
+		for num%i == 0 {
+			factors = append(factors, i)
+			num /= i
+		}
+	}
+
+	return factors
+}
