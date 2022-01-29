6f906de8abbd05f5a04b92b009b00d1726c3a262
Stuart Garner <stuart@radialaspect.com>
2022-01-28 22:45:50 -0500

    switching iterator methods

--- rust/low-power-embedded-game/src/lib.rs
+++ rust/low-power-embedded-game/src/lib.rs
@@ -6,7 +6,7 @@ pub fn divmod(dividend: i16, divisor: i16) -> (i16, i16) {
 }
 
 pub fn evens<T>(iter: impl Iterator<Item = T>) -> impl Iterator<Item = T> {
-    iter.enumerate().filter(|x| x.0 % 2 == 0).map(|x| x.1)
+    iter.step_by(2)
 }
 
 pub struct Position(pub i16, pub i16);
