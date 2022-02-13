e4a7bb2f4046579f169a3d6aa0755276482e0177
Stuart Garner <stuart@radialaspect.com>
2022-02-13 15:47:19 -0500

    Updating rust macros exercise

--- rust/macros/src/lib.rs
+++ rust/macros/src/lib.rs
@@ -3,7 +3,7 @@ macro_rules! hashmap {
     ( $( $($k:expr => $v:expr)+ $(,)? )* ) => {
         {
             let mut temp_map = ::std::collections::HashMap::new();
-            $($(temp_map.insert($k, $v);)*)*
+            $($(temp_map.insert($k, $v);)+)*
             temp_map
         }
     };
